c839f5be2863cd2600ff5b6e50d643422d9df04e
HimanshuKumarDutt094 <adasimobenio@gmail.com>
2024-02-04 19:32:57 +0530

    Fixing svg

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,13 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/Header";
 import { ThemeProvider } from "@/components/context/theme-provider";
-import Head from "next/head";
 import Logo from '@/public/icons/InternhubLogo.svg'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "InternHub",
   description: "IINTM Placement cell",
+  icons: Logo
 };
 
 export default function RootLayout({
@@ -20,9 +20,7 @@ export default function RootLayout({
   return (
 
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" href={Logo} type="image/svg+xml" />
-      </Head>
+
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system">
           <Header />
