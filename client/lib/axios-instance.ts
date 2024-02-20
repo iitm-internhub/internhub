af248922f1211617f68ffbc4bc05c6647db0b0cd
Himanshu Kumar Dutt <adasimobenio@gmail.com>
2024-02-20 11:43:31 +0000

    fetching master

--- client/lib/axios-instance.ts
+++ client/lib/axios-instance.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:8080/"
+      ? "https://glorious-succotash-55xvj5vg64j2pvrj-8080.app.github.dev/"
       : "https://tiny-pig-scrubs.cyclic.app/",
   timeout: 5000,
 });
