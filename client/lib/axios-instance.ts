3c06c0c32318084defdec41c21d81eccd8bad12c
Himanshu Kumar Dutt <adasimobenio@gmail.com>
2024-02-21 21:36:05 +0530

    Update axios-instance.ts

--- client/lib/axios-instance.ts
+++ client/lib/axios-instance.ts
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http:localhost:8080" 
-      : "https://tender-cod-swimsuit.cyclic.app/",
+      : "http://internhub-server.ap-south-1.elasticbeanstalk.com/",
   timeout: 5000,
 });
 
