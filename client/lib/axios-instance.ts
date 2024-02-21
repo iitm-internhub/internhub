4106cc052c62a3c4d48460600e9df3a0329d7e25
AbhishekSingh <siyanisabhishek@gmail.com>
2024-02-21 12:56:13 +0530

    changed server url

--- client/lib/axios-instance.ts
+++ client/lib/axios-instance.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // http://localhost:8080
-// https://tiny-pig-scrubs.cyclic.app/
+// https://tender-cod-swimsuit.cyclic.app/
 
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8080/"
-      : "https://tiny-pig-scrubs.cyclic.app/",
+      : "https://tender-cod-swimsuit.cyclic.app/",
   timeout: 5000,
 });
 
